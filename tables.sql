138d20f508f9fc8cb59ec7784ba870603b566834
Jonathan Lesueur <jonathan.lesueur.tic@gmail.com>
2020-11-16 01:57:48 +0100

    car-branch: MàJ tables et modifications DataBaseService

--- tables.sql
+++ tables.sql
@@ -11,3 +11,17 @@ INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `birthday`) VALUES
 (1, 'Vincent', 'Godé', 'hello@vincentgo.fr', '1990-11-08'),
 (2, 'Albert', 'Dupond', 'sonemail@gmail.com', '1985-11-08'),
 (3, 'Thomas', 'Dumoulin', 'sonemail2@gmail.com', '1985-10-08');
+
+CREATE TABLE `cars` (
+  `id` int AUTO_INCREMENT NOT NULL,
+  `marque` tinytext NOT NULL,
+  `couleur` tinytext NOT NULL,
+  `mise_circulation` datetime NOT NULL,
+  `puissance_moteur` int(11) NOT NULL,
+  `modele` tinytext NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `cars` (`id`, `marque`, `couleur`, `mise_circulation`, `puissance_moteur`, `modele`) VALUES
+(1, 'Peugeot', 'Blanc', '2020-11-14 20:56:27', 120, '306 HDI'),
+(2, 'Citroen', 'Bleu', '2020-11-15 00:00:00', 150, 'C6');
