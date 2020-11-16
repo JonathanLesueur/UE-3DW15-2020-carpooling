1d277afdda46b0bc1eaa957306fc9c2c6ddbd64b
Blondin Marylou <blondin.marylou@etu.unilim.fr>
2020-11-16 17:36:50 +0100

    reservations-branch: suite du CRUD des réservations

---
+++ reservation_create.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Controllers\ReservationsController;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$controller = new ReservationsController();
+echo $controller->createReservation();
+?>
+
+<p>Création d'une réservation</p>
+<form method="post" action="reservations_create.php" name="reservationCreateForm">
+    <label for="utilisateur">Utilisateur :<br><input name="utilisateur" id="utilisateur" type="text"></label><br>
+    <label for="date_depart">Date du départ (format dd-mm-yyyy) :<br><input name="date_depart" id="date_depart" type="text"></label><br>
+    <label for="lieu_depart">Lieu du départ :<br><input name="lieu_depart" id="lieu_depart" type="text"></label><br>
+    <label for="lieu_arrivee">Lieu de l'arrivée :<br><input name="lieu_arrivee" id="lieu_arrivee" type="date"></label><br>
+    <input type="submit" value="Créer une réservation">
+</form>
---
+++ reservation_update.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Controllers\ReservationsController;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$controller = new ReservationsController();
+echo $controller->updateReservation();
+?>
+
+<p>Mise à jour d'une réservation</p>
+<form method="post" action="reservation_update.php" name ="reservationUpdateForm">
+    <label for="id">Id :</label>
+    <input type="text" name="id">
+    <br />
+    <label for="utilisateur">Utilisateur :</label>
+    <input type="text" name="utilisateur">
+    <br />
+    <label for="date_depart">Date du départ (format dd-mm-yyyy) :</label>
+    <input type="text" name="date_depart">
+    <br />
+    <label for="lieu_depart">Lieu du départ :</label>
+    <input type="text" name="lieu_depart">
+    <br />
+    <label for="lieu_arrivee">Lieu de l'arrivée :</label>
+    <input type="text" name="lieu_arrivee" id="lieu_arrivee">
+    <br />
+    <input type="submit" value="Modifier la réservation">
+</form>
